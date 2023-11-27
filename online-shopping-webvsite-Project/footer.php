68cfe55c172a48479d1dd08d041f8b231b9e1041
Piyushp2410 <141002901+Piyushp2410@users.noreply.github.com>
2023-11-27 11:00:48 +0530

    Add files via upload

---
+++ online-shopping-webvsite-Project/footer.php
@@ -0,0 +1,10 @@
+<html>
+<footer class="footer">
+            <div class="container">
+                <center>
+                    <p>Copyright &copy All Rights Reserved.</p>
+                    <p>This website is developed by Piyush Parmar</p>
+                </center>
+            </div>
+        </footer>
+        </html>
